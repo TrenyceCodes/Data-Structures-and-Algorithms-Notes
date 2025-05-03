af7783e77ef2a884e1928821108044a04156dd79
Neice <52137719+TrenyceCodes@users.noreply.github.com>
2025-05-02 22:32:19 -0400

    Finished setting up BigOLogN notes

---
+++ BigO-Notation/BigOLogN.py
@@ -0,0 +1,33 @@
+
+def BinarySearch(target: int, nums: list[int]) -> int:
+    #low is the first number in the array
+    #high is the last number in the array
+    #these are considered two pointers
+    low = 0
+    high = len(nums)-1
+
+    while low <= high:
+        #handles splitting the array in half,
+        #choosing a number
+        middle = low +(high-low)//2
+        print(middle)
+
+        if nums[middle] == target:
+            #returns middle index that target is found
+            return middle 
+        elif nums[middle] < target:
+            low = middle + 1
+        else:
+            high = middle - 1
+
+    return -1
+
+if __name__ == "__main__":
+    nums = [1, 2, 4, 5, 7]
+    target = 1
+
+    result = BinarySearch(target=target, nums=nums)
+    if result != -1:
+        print(f"Element is present at index {result}")
+    else:
+        print("Element is not present in array")
